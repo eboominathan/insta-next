95bd959860a16d344b9dbf74bcab28d3524fd439
Boominathan <eboominathan@gmail.com>
2024-04-08 07:05:14 +0530

    modal added with icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,16 @@
  import  Link   from 'next/link';
  import Image from 'next/image';
  import { signIn, useSession, signOut } from 'next-auth/react';
+ import Modal from 'react-modal';
  import { IoMdAddCircleOutline } from 'react-icons/io';
 import { HiCamera } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
+
 
  export default function Header() {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
@@ -64,6 +68,47 @@ import { AiOutlineClose } from 'react-icons/ai';
           </button>
         )}
         </div>
+
+        {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          className='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md'
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <div className='flex flex-col justify-center items-center h-[100%]'>
+           
+              <HiCamera
+                onClick={() => filePickerRef.current.click()}
+                className='text-5xl text-gray-400 cursor-pointer'
+              />            
+            
+          </div>
+          <input
+            type='text'
+            maxLength='150'
+            placeholder='Please enter you caption...'
+            className='m-4 border-none text-center w-full focus:ring-0 outline-none'
+            onChange={(e) => setCaption(e.target.value)}
+          />
+          <button
+            // onClick={handleSubmit}
+            // disabled={
+            //   !selectedFile ||
+            //   caption.trim() === '' ||
+            //   postUploading ||
+            //   imageFileUploading
+            // }
+            className='w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100'
+          >
+            Upload Post
+          </button>
+          <AiOutlineClose
+            className='cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300'
+            onClick={() => setIsOpen(false)}
+          />
+        </Modal>
+      )}
     </div>
   )
 }
